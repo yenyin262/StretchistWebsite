f726f7f3bb327d6eba253b3fcc665ac594b3b7a9
yenyin262 <yenyinf287@gmail.com>
2021-07-18 17:10:24 -0700

    Clean up Navbar

--- components/NavBar/NavBar.js
+++ components/NavBar/NavBar.js
@@ -2,17 +2,13 @@ import Link from "next/link";
 import styles from "./NavBar.module.css";
 import logo from "../../public/logo.svg";
 import togglebtn from "../../public/icon-hamburger.svg";
-import closeIcon from "../../public/close_icon.png";
-import dropdownBtn from "../../public/down-arrow.png";
 import Image from "next/image";
 
 import { useState } from "react";
-import Dropdown from "../Dropdown/dropdown";
 
 const NavBar = () => {
   // set state of the menu icon
   const [click, setClick] = useState(false);
-  const [dropdown, setDropdown] = useState(false);
 
   // create function to click on menu icon
   const handleClick = () => {
@@ -23,37 +19,16 @@ const NavBar = () => {
     setClick(false);
   };
 
-  // const onToggle = () => {
-  //   if (window.innerWidth < 960) {
-  //     setDropdown(false);
-  //   } else {
-  //     setDropdown(true);
-  //   }
-  // };
-
-  // const onToggleLeave = () => {
-  //   if (window.innerWidth < 960) {
-  //     setDropdown(false);
-  //   } else {
-  //     setDropdown(false);
-  //   }
-  // };
-
   return (
     <div className={styles.container}>
       <div className={styles.logo}>
         <Link href="/">
-          <Image src={logo} alt="logo" width={150} height={40} />
+          <Image src={logo} alt="Logo" width={150} height={40} />
         </Link>
       </div>
       <div className={styles.togglebtn} onClick={handleClick}>
         <Link href="/">
-          <Image
-            src={click ? closeIcon : togglebtn}
-            alt="togglebutton"
-            width={30}
-            height={20}
-          />
+          <Image src={togglebtn} alt="Menu icon" width={30} height={20} />
         </Link>
       </div>
       <div className={styles.navBarLinks}>
@@ -62,16 +37,10 @@ const NavBar = () => {
             <a className={styles.link}> About</a>
           </Link>
         </div>
-        <div
-          className={styles.servicesContainer}
-          // onMouseEnter={onToggle}
-          // onMouseLeave={onToggleLeave}
-        >
+        <div className={styles.servicesContainer}>
           <Link href="" onClick={closeMobileMenu}>
             <a className={styles.link}>Services</a>
           </Link>
-          <Image src={dropdownBtn} alt="btn" width={40} height={40} />
-          {dropdown && <Dropdown />}
         </div>
         <div>
           <Link href="/about" onClick={closeMobileMenu}>
