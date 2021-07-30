c5ad4df794317f18e0bb88b37b6e7d216978a710
yenyin262 <yenyinf287@gmail.com>
2021-07-29 20:52:56 -0700

    Fix main section styles, and update sectionjson file

--- components/MainSection/MainSection.js
+++ components/MainSection/MainSection.js
@@ -6,45 +6,43 @@ const MainSection = ({ data }) => {
   return (
     <>
       <style jsx>{`
-        @media (max-width: 400px) {
-          a {
-            padding: 30px 0 8px;
-            position: relative;
-          }
-
-          a::before {
-            content: "";
-            position: absolute;
-            left: 65px;
-            bottom: 10px;
-            width: 130px;
-            height: 7px;
-            transform: skew(-12deg) translateX(-50%);
-            background: ${data.linkColor};
-            z-index: -1;
-          }
+        a {
+          padding: 30px 0 8px;
+          position: relative;
         }
 
-        @media (min-width: 400px) {
+        a::before {
+          content: "";
+          position: absolute;
+          left: 65px;
+          bottom: 10px;
+          width: 145px;
+          height: 8px;
+          transform: skew(-12deg) translateX(-50%);
+          background: ${data.linkColor};
+          z-index: -1;
         }
       `}</style>
       <div className={styles.content_container}>
         <div className={styles.imgContainer}>
           <Image
             src={data.image}
-            alt="dd"
+            alt={data.altDescription}
             width={370}
             height={370}
             layout="responsive"
           />
         </div>
+
         <div className={styles.mainContent_block}>
-          <div className={styles.title}>{data.title}</div>
-          <div className={styles.content}>{data.content}</div>
-          <div className={styles.content_btn}>
-            <Link href="/about">
-              <a>{data.details}</a>
-            </Link>
+          <div className={styles.contentItem}>
+            <div className={styles.title}>{data.title}</div>
+            <div className={styles.content}>{data.content}</div>
+            <div className={styles.content_btn}>
+              <Link href="/about">
+                <a>{data.details}</a>
+              </Link>
+            </div>
           </div>
         </div>
       </div>
