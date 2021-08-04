2a8a38d88766d200953d6ac775a16d47ad25a232
yenyin262 <yenyinf287@gmail.com>
2021-08-03 21:37:08 -0700

    Add togglebutton component

--- components/NavBar/NavBar.js
+++ components/NavBar/NavBar.js
@@ -1,11 +1,11 @@
 import Link from "next/link";
 import styles from "./NavBar.module.css";
 import logo from "../../public/logo.svg";
-import togglebtn from "../../public/icon-hamburger.svg";
 import Image from "next/image";
 import NavItems from "../NavItems/NavItems";
 import MobileMenu from "../MobileMenu/MobileMenu";
 import useMobilenavmenu from "./useMobilenavmenu";
+import ToggleButton from "../ToggleButton/ToggleButton";
 
 const NavBar = () => {
   const { ref, mobileNavmenu, setMobileNavmenu } = useMobilenavmenu(false);
@@ -23,12 +23,12 @@ const NavBar = () => {
           />
         </Link>
       </div>
-      <div className={styles.togglebtn} ref={ref}>
+      <div ref={ref}>
         <button
           className={styles.btn_menu}
           onClick={() => setMobileNavmenu(!mobileNavmenu)}
         >
-          <Image src={togglebtn} alt="Menu icon" width={30} height={20} />
+          <ToggleButton />
         </button>
 
         {mobileNavmenu && (
