026557a7719bc6a0f387c009909ab4b92f040325
yenyin262 <yenyinf287@gmail.com>
2021-08-05 13:52:06 -0700

    Update Footer Nav List styles

--- components/FooterNavItems/FooterNavItems.js
+++ components/FooterNavItems/FooterNavItems.js
@@ -4,11 +4,11 @@ import Link from "next/link";
 
 const FooterNavItems = () => {
   return (
-    <div className={styles.navBarLinks}>
+    <div className={styles["footer-nav__list"]}>
       {items.map((item, index) => {
         return (
           <Link href={item.link} key={index}>
-            <a className={styles.link}> {item.title}</a>
+            <a className={styles["footer-nav__link"]}> {item.title}</a>
           </Link>
         );
       })}
