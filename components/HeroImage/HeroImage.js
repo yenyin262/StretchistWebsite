c05d1f0479c056183e8c5a3c9173dab64e34690f
yenyin262 <yenyinf287@gmail.com>
2021-08-17 09:39:12 -0700

    update hero content

--- components/HeroImage/HeroImage.js
+++ components/HeroImage/HeroImage.js
@@ -2,10 +2,11 @@ import styles from "./HeroImage.module.css";
 import heroImagedata from "../../data/heroImageData.json";
 import classnames from "classnames";
 import HeroArrow from "../HeroArrow/HeroArrow";
+import Image from "next/Image";
 const HeroImage = ({ children }) => {
   return (
     <>
-      <style jsx>{`
+      {/* <style jsx>{`
         @media (max-width: 400px) {
           .heroImage {
             background-image: url("${heroImagedata.images.mobile}");
@@ -16,13 +17,29 @@ const HeroImage = ({ children }) => {
             background-image: url("${heroImagedata.images.desktop}");
           }
         }
+      `}</style> */}
+
+      <style jsx>{`
+        @media (max-width: 400px) {
+          .heroImage {
+            background: #112b24;
+          }
+        }
+        @media (min-width: 400px) {
+          .heroImage {
+            background: #112b24;
+          }
+        }
       `}</style>
 
       <div className={classnames(`heroImage`, styles.hero)}>
         {children}
         <div className={styles.hero__content}>
-          <h1 className={styles.hero__content_title}>We are creatives</h1>
-          <HeroArrow />
+          {/* <Image src="/logo2.png" width={375} height={146} /> */}
+          {/* <h1 className={styles.hero__content_title}>We are creatives</h1>
+          <HeroArrow /> */}
+          <h1 style={{ color: "white" }}>Assisted Stretch Therapy</h1>
+          <h3 style={{ color: "hsl(156deg 50% 48%)" }}>with Gaby Chin</h3>
         </div>
       </div>
     </>
