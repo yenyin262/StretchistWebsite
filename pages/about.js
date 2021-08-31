b34f222eaef19b1a6f7486da5a8bc373eeb3e29f
yenyin262 <yenyinf287@gmail.com>
2021-08-31 14:38:14 -0700

    add about component

--- pages/about.js
+++ pages/about.js
@@ -1,14 +1,55 @@
-import Link from "next/link";
+import styles from "./about.module.css";
 import Layout from "../components/Layout/Layout";
-const About = () => {
+import { getJSONAboutData } from "../lib/about";
+import Image from "next/Image";
+const About = ({ coreValues, myExperience }) => {
   return (
     <Layout>
-      <h4>About Me </h4>
-      <Link href="/">
-        <a> Go back to Home</a>
-      </Link>
+      <div>
+        <h1 className={styles.aboutme_mainheading}>About Me</h1>
+
+        <div>
+          <h2 className={styles.corevalues__subheading}>{coreValues.title}</h2>
+          <div className={styles.corevalue__list}>
+            {coreValues.values.map((value, index) => {
+              return (
+                <div key={index} className={styles.corevalueItem}>
+                  <Image src={value.mobile} height={100} width={100} />
+                  <div className={styles.corevalue__label}>{value.label}</div>
+                  <div className={styles.description}>{value.description}</div>
+                </div>
+              );
+            })}
+          </div>
+          <div>
+            <h2 className={styles.myexperience__subheading}>
+              {myExperience.title}
+            </h2>
+            <div className={styles.myexperience__list}>
+              {myExperience.values.map((value, index) => {
+                return (
+                  <div className={styles.myexperienceItem} key={index}>
+                    <h2 className={styles.numbers}>{value.numbers}</h2>
+                    <div className={styles.myexperience__label}>
+                      {value.label}
+                    </div>
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+        </div>
+      </div>
     </Layout>
   );
 };
 
 export default About;
+
+export async function getStaticProps() {
+  const { coreValues, myExperience } = getJSONAboutData();
+
+  return {
+    props: { coreValues, myExperience },
+  };
+}
