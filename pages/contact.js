2e3a7bfbd32e4aab4e46db3a513d3f60cfa6fa88
yenyin262 <yenyinf287@gmail.com>
2021-08-31 14:38:34 -0700

    add contact component

--- pages/contact.js
+++ pages/contact.js
@@ -1,5 +1,34 @@
+import Layout from "../components/Layout/Layout";
+import styles from "./contact.module.css";
+import Image from "next/image";
 const Contact = () => {
-  return <div>Contact</div>;
+  return (
+    <Layout>
+      <div style={{ margin: "20px", height: "50vh" }}>
+        <h2 className={styles.heading}>Contact Us</h2>
+        <div
+          style={{
+            width: "100%",
+            marginTop: "20px",
+            backgroundColor: "#8fd4c5",
+            maxWidth: "500px",
+            margin: " 0 auto",
+          }}
+        >
+          {/* <Image src={"/g.jpg"} height={540} width={400} /> */}
+          <p
+            style={{
+              textAlign: "center",
+              padding: "30px",
+            }}
+          >
+            For general inquries and partnership opportunities, please email{" "}
+            <span style={{ fontWeight: "bold" }}> thestretchist@gmail.com</span>
+          </p>
+        </div>
+      </div>
+    </Layout>
+  );
 };
 
 export default Contact;
