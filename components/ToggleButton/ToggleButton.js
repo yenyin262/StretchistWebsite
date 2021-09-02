e3d42d05a67da03506c54ae7f3bc25233b94c8fa
yenyin262 <yenyinf287@gmail.com>
2021-09-02 15:35:04 -0700

    change toggle btn color

--- components/ToggleButton/ToggleButton.js
+++ components/ToggleButton/ToggleButton.js
@@ -3,7 +3,7 @@ const ToggleButton = () => {
     <svg width="24" height="18" xmlns="http://www.w3.org/2000/svg">
       <path
         d="M24 16v2H0v-2h24zm0-8v2H0V8h24zm0-8v2H0V0h24z"
-        fill="#FFF"
+        fill="#707271"
         fill-rule="evenodd"
       />
     </svg>
