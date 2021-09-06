b64ee09f66e32f517318ed57656cfc1776f379b4
yenyin262 <yenyinf287@gmail.com>
2021-09-06 14:44:13 -0700

    fix fill rule prop error

--- components/ToggleButton/ToggleButton.js
+++ components/ToggleButton/ToggleButton.js
@@ -4,7 +4,7 @@ const ToggleButton = () => {
       <path
         d="M24 16v2H0v-2h24zm0-8v2H0V8h24zm0-8v2H0V0h24z"
         fill="#707271"
-        fill-rule="evenodd"
+        fillRule="evenodd"
       />
     </svg>
   );
