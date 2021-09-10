be92e32b92e36d7bd79d616dfaa6f4ff0a62f9f3
yenyin262 <yenyinf287@gmail.com>
2021-09-09 20:19:54 -0700

    update contact page content

--- pages/contact.js
+++ pages/contact.js
@@ -1,11 +1,11 @@
 import Layout from "../components/Layout/Layout";
 import styles from "./contact.module.css";
-import Image from "next/image";
-const Contact = () => {
+import { getJSONContactData } from "../lib/contact";
+const Contact = ({ title, message, email }) => {
   return (
     <Layout>
       <div style={{ margin: "20px", height: "50vh" }}>
-        <h2 className={styles.heading}>Contact Us</h2>
+        <h2 className={styles.heading}>{title}</h2>
         <div
           style={{
             width: "100%",
@@ -15,15 +15,13 @@ const Contact = () => {
             margin: " 0 auto",
           }}
         >
-          {/* <Image src={"/g.jpg"} height={540} width={400} /> */}
           <p
             style={{
-              textAlign: "center",
               padding: "30px",
+              textAlign: "center",
             }}
           >
-            For general inquries and partnership opportunities, please email{" "}
-            <span style={{ fontWeight: "bold" }}> thestretchist@gmail.com</span>
+            {message} <span style={{ fontWeight: "bold" }}> {email} </span>
           </p>
         </div>
       </div>
@@ -32,3 +30,9 @@ const Contact = () => {
 };
 
 export default Contact;
+
+export async function getStaticProps() {
+  const props = getJSONContactData();
+
+  return { props };
+}
