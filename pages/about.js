46ea9da3fb643f2e7b7f4945d3f584e94dd99d75
yenyin262 <yenyinf287@gmail.com>
2021-09-09 20:25:50 -0700

    update about component

--- pages/about.js
+++ pages/about.js
@@ -2,11 +2,11 @@ import styles from "./about.module.css";
 import Layout from "../components/Layout/Layout";
 import { getJSONAboutData } from "../lib/about";
 import Image from "next/Image";
-const About = ({ coreValues, myExperience }) => {
+const About = ({ coreValues, myExperience, heading }) => {
   return (
     <Layout>
       <div>
-        <h1 className={styles.aboutme_mainheading}>About Me</h1>
+        <h1 className={styles.aboutme_mainheading}>{heading}</h1>
 
         <div>
           <h2 className={styles.corevalues__subheading}>{coreValues.title}</h2>
@@ -51,10 +51,18 @@ const About = ({ coreValues, myExperience }) => {
 
 export default About;
 
+// export async function getStaticProps() {
+//   const { aboutMe } = getJSONAboutData();
+
+//   return {
+//     props: { aboutMe },
+//   };
+// }
+
 export async function getStaticProps() {
-  const { coreValues, myExperience } = getJSONAboutData();
+  const { coreValues, myExperience, heading } = getJSONAboutData();
 
   return {
-    props: { coreValues, myExperience },
+    props: { coreValues, myExperience, heading },
   };
 }
