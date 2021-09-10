f84d7b01f567ed136a3ea88e47b929dcff163a06
yenyin262 <yenyinf287@gmail.com>
2021-09-09 20:16:44 -0700

    update styles

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -1,6 +1,7 @@
 import styles from "./Footer.module.css";
 import Logo from "../Logo";
 import FooterNavItems from "../FooterNavItems/FooterNavItems";
+import Image from "next/Image";
 
 import Icons from "../Icons/Icons";
 const Footer = () => {
@@ -8,7 +9,14 @@ const Footer = () => {
     <div className={styles.footer}>
       <div className={styles.footer__content}>
         <div className={styles.footer__logo}>
-          <Logo />
+          <Image
+            src="/TheStretchistLogo.png"
+            alt="Logo"
+            layout="responsive"
+            width={700}
+            height={378}
+            color={"black"}
+          />
         </div>
         <FooterNavItems />
         <div>
