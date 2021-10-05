3e34470109ad807ee6b71af2ca24f5db88634baf
yenyin262 <yenyinf287@gmail.com>
2021-10-04 17:59:48 -0700

    update styles

--- pages/about.js
+++ pages/about.js
@@ -14,56 +14,48 @@ const About = ({ preview, coreValues, myExperience, title, aboutSection }) => {
         }
       `}</style>
       <Layout preview={preview}>
-        <div>
-          <h1 className={styles.aboutme_mainheading}>{title}</h1>
+        <h1 className={styles.aboutme_mainheading}>{title}</h1>
+        <div className={styles.imgContainer}>
+          <ProfileImgSlider
+            images={aboutSection.images}
+            content={aboutSection.content}
+          />
+        </div>
 
-          <div>
-            <div className={styles.imgContainer}>
-              <ProfileImgSlider
-                images={aboutSection.images}
-                content={aboutSection.content}
-              />
-            </div>
-
-            <h2 className={styles.corevalues__subheading}>
-              {coreValues.title}
-            </h2>
-
-            <div className={styles.corevalue__list}>
-              {coreValues.values.map((value, index) => {
-                return (
-                  <div key={index} className={styles.corevalueItem}>
-                    <Image
-                      src={value.image}
-                      height={100}
-                      width={100}
-                      alt={value.alt}
-                    />
-                    <div className={styles.corevalue__label}>{value.label}</div>
-                    <div className={styles.description}>
-                      {value.description}
-                    </div>
+        <div className={styles.contentContainer}>
+          <h2 className={styles.corevalues__subheading}>{coreValues.title}</h2>
+          <div className={styles.corevalue__list}>
+            {coreValues.values.map((value, index) => {
+              return (
+                <div key={index} className={styles.corevalueItem}>
+                  <Image
+                    src={value.image}
+                    height={100}
+                    width={100}
+                    alt={value.alt}
+                  />
+                  <div className={styles.corevalue__label}>{value.label}</div>
+                  <div className={styles.description}>{value.description}</div>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+        <div className={styles.experienceContentContainer}>
+          <h2 className={styles.myexperience__subheading}>
+            {myExperience.title}
+          </h2>
+          <div className={styles.myexperience__list}>
+            {myExperience.values.map((value, index) => {
+              return (
+                <div className={styles.myexperienceItem} key={index}>
+                  <h2 className={styles.numbers}>{value.numbers}</h2>
+                  <div className={styles.myexperience__label}>
+                    {value.label}
                   </div>
-                );
-              })}
-            </div>
-            <div>
-              <h2 className={styles.myexperience__subheading}>
-                {myExperience.title}
-              </h2>
-              <div className={styles.myexperience__list}>
-                {myExperience.values.map((value, index) => {
-                  return (
-                    <div className={styles.myexperienceItem} key={index}>
-                      <h2 className={styles.numbers}>{value.numbers}</h2>
-                      <div className={styles.myexperience__label}>
-                        {value.label}
-                      </div>
-                    </div>
-                  );
-                })}
-              </div>
-            </div>
+                </div>
+              );
+            })}
           </div>
         </div>
       </Layout>
