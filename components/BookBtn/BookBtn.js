d4713de6d5c9092f907be7bc93d9e77069869133
yenyin262 <yenyinf287@gmail.com>
2021-11-22 20:24:27 -0800

    remove a tags

--- components/BookBtn/BookBtn.js
+++ components/BookBtn/BookBtn.js
@@ -4,18 +4,11 @@ import styles from "./BookBtn.module.css";
 export default function BookBtn() {
   return (
     <div className={styles[`book-btn`]}>
-      <a
-        href="https://calendly.com/thestretchist/book-your-session"
-        target="_blank"
-        rel="noreferrer"
-        className={styles.link}
-      >
-        <span></span>
-        <span></span>
-        <span></span>
-        <span></span>
-        Book Now
-      </a>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      Book Now
     </div>
   );
 }
